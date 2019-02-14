286d62bd868fe3d33f56a311d222cef0675c1f72
hyoni0817 <hyoni0817@naver.com>
2019-02-14 23:55:06 +0900

    TodoForm 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/Home.js';
+import TodoForm from './components/home/TodoForm.js';
 
 class App extends Component {
   render() {
     return (
+      <Fragment>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
         <div>
           <Home></Home>
         </div>
+        <TodoForm></TodoForm>
+      </Fragment>
+
 
         
 
