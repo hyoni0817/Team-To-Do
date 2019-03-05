15d00fe3e13c372ee6569d6b0d70dbab2343e3dc
hyoni0817 <hyoni0817@naver.com>
2019-03-06 01:29:37 +0900

    EMERGENCY에서 EMERGENCY_FL로 변경

--- server/model/todo.js
+++ server/model/todo.js
@@ -5,7 +5,7 @@ Todo.getTodoList = function(callback) {
     pool.getConnection ((err, conn) => {
         if(err) return callback(err, null);
 
-        const sql = 'select WRITE_NO, TITLE, DATE_FORMAT(END_DT,"%Y-%m-%d") as END_DT, PARTICIPANT, EMERGENCY from todolist_tb order by EMERGENCY desc, END_DT asc';
+        const sql = 'select WRITE_NO, TITLE, DATE_FORMAT(END_DT,"%Y-%m-%d") as END_DT, PARTICIPANT, EMERGENCY_FL from todolist_tb order by EMERGENCY_FL desc, END_DT asc';
         conn.query(sql, (err, result) => {
             console.log(result);
             callback(null, result)
