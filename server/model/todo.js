64eee2f97e8203de06e26b4e108e25b39a4befe7
hyoni0817 <hyoni0817@naver.com>
2019-03-07 01:11:51 +0900

    커넥션 반납 코드 추가

--- server/model/todo.js
+++ server/model/todo.js
@@ -3,11 +3,15 @@ class Todo {}
 
 Todo.getTodoList = function(callback) {
     pool.getConnection ((err, conn) => {
-        if(err) return callback(err, null);
+        if(err){
+            conn.release();
+            return callback(err, null);
+        } 
 
         const sql = 'select WRITE_NO, TITLE, DATE_FORMAT(END_DT,"%Y-%m-%d") as END_DT, PARTICIPANT, EMERGENCY_FL from todolist_tb order by EMERGENCY_FL desc, END_DT asc';
         conn.query(sql, (err, result) => {
             console.log(result);
+            conn.release();
             callback(null, result)
         })
     })
